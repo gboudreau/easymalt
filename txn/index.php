563aff92fc6673bc119ae9ce44ac6506f89aaa50
Guillaume Boudreau <guillaume@pommepause.com>
2017-02-14 11:36:10 -0500

    New feature: create post-processing rules when editing a transaction

--- txn/index.php
+++ txn/index.php
@@ -5,7 +5,17 @@ require 'init.inc.php';
 
 $q = "SELECT * FROM transactions WHERE id = :id";
 $txn = DB::getFirst($q, (int) @$_REQUEST['id']);
-$txn->tags = explode(',', $txn->tags);
+if (empty($txn->tags)) {
+    $txn->tags = [];
+} else {
+    $txn->tags = explode(',', $txn->tags);
+}
+if ($txn->display_name === NULL) {
+    $txn->display_name = '';
+}
+if ($txn->category === NULL) {
+    $txn->category = '';
+}
 
 if (empty($txn)) {
     die("Transaction (ID " . @$_REQUEST['id'] . ") not found.");
@@ -32,6 +42,38 @@ if (isset($_POST['id'])) {
 
     $q = "UPDATE transactions SET " . implode(', ', $updates) . " WHERE id = :id";
     DB::execute($q, $params);
+
+    if ($_POST['post_processing_rule'] == 'true') {
+        $matches_name = $_POST['post_processing_rule_matching'];
+        if (@$_POST['post_processing_rule_matching_format'] != 'regex') {
+            $matches_name = preg_quote($matches_name);
+        }
+        $updates = [];
+        $params = [];
+        if (@$_POST['post_processing_rule_desc'] == 'true') {
+            $updates[] = 'display_name = :display_name';
+            $params['display_name'] = $_POST['display_name'];
+        }
+        if (@$_POST['post_processing_rule_category'] == 'true') {
+            $updates[] = 'category = :category';
+            $params['category'] = $_POST['category'];
+        }
+        if (@$_POST['post_processing_rule_tags'] == 'true') {
+            $updates[] = 'tags = :tags';
+            $params['tags'] = empty($_POST['tags']) ? NULL : implode(',', $_POST['tags']);
+        }
+        $q = "INSERT INTO post_processing SET regex = :regex, " . implode(", ", $updates) . " ON DUPLICATE KEY UPDATE " . implode(", ", $updates);
+        DB::insert($q, array_merge($params, ['regex' => $matches_name]));
+
+        $q = "SELECT id FROM transactions WHERE `name` REGEXP :regex";
+        $txn_ids = DB::getAllValues($q, $matches_name);
+
+        $updates[] = "post_processed = 'yes'";
+        foreach ($txn_ids as $txn_id) {
+            $q = "UPDATE transactions SET " . implode(', ', $updates) . " WHERE id = :id";
+            DB::execute($q, array_merge($params, ['id' => $txn_id]));
+        }
+    }
 
     header('Location: ' . $_POST['goto']);
     exit(0);
@@ -83,19 +125,57 @@ $cats = DB::getAllValues($q);
             <td>
                 Tags:<br/>
                 <?php foreach ($tags as $tag) : ?>
-                    <input type="checkbox" id="tag_<?php echo he($tag) ?>" name="tags[]" value="<?php phe($tag) ?>" <?php if (array_contains($txn->tags, $tag)) { echo 'checked="checked"'; } ?>/>
+                    <input type="checkbox" id="tag_<?php echo he($tag) ?>" name="tags[]" value="<?php phe($tag) ?>" <?php echo_if(array_contains($txn->tags, $tag), 'checked="checked"') ?>/>
                     <label for="tag_<?php echo he($tag) ?>"><?php phe($tag) ?></label>
                 <?php endforeach; ?>
             </td>
         </tr>
         <tr>
             <td>
-                Category:<br/>
-                <select name="category">
+                <label for="category">Category:</label><br/>
+                <select id="category" name="category">
                 <?php foreach ($cats as $cat) : ?>
-                    <option value="<?php phe($cat) ?>" <?php if ($txn->category == $cat) { echo 'selected="selected"'; } ?>><?php phe($cat) ?></option>
+                    <option value="<?php phe($cat) ?>" <?php echo_if($txn->category == $cat, 'selected="selected"') ?>><?php phe($cat) ?></option>
                 <?php endforeach; ?>
                 </select>
+            </td>
+        </tr>
+        <tr>
+            <td>
+               <div class="apply_to_all">
+                   <div>
+                       <input type="checkbox" id="post_processing_rule" name="post_processing_rule" value="true" />
+                       <label for="post_processing_rule">
+                           Apply to all transactions whose name contains:
+                       </label>
+                       <input type="text" name="post_processing_rule_matching" value="<?php phe($txn->name) ?>" />
+                       (<label for="post_processing_rule_matching_format">is RegExp</label>: <input type="checkbox" id="post_processing_rule_matching_format" name="post_processing_rule_matching_format" value="regex" />)
+                   </div>
+                   <ul>
+                       <li class="desc">
+                           <input type="checkbox" id="post_processing_rule_desc" name="post_processing_rule_desc" value="true" />
+                           <label for="post_processing_rule_desc">
+                               Short description: <span class="value"></span>
+                           </label>
+                       </li>
+                   </ul>
+                   <ul>
+                       <li class="tags">
+                           <input type="checkbox" id="post_processing_rule_tags" name="post_processing_rule_tags" value="true" />
+                           <label for="post_processing_rule_tags">
+                               Tags: <span class="value"></span>
+                           </label>
+                       </li>
+                   </ul>
+                   <ul>
+                       <li class="category">
+                           <input type="checkbox" id="post_processing_rule_category" name="post_processing_rule_category" value="true" />
+                           <label for="post_processing_rule_category">
+                               Category: <span class="value"></span>
+                           </label>
+                       </li>
+                   </ul>
+               </div>
             </td>
         </tr>
         <tr>
@@ -103,5 +183,73 @@ $cats = DB::getAllValues($q);
         </tr>
     </table>
 </form>
+<script
+        src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+        crossorigin="anonymous"></script>
+<script>
+    $(function() {
+        $('input, select').on('change', function() {
+            displayApplyToAll()
+        });
+        $('input[type=text]').on('input', function() {
+            displayApplyToAll()
+        });
+    });
+    var initial_desc = <?php pjs($txn->display_name) ?>;
+    var initial_tags = <?php pjs($txn->tags) ?>;
+    var initial_category = <?php pjs($txn->category) ?>;
+    function displayApplyToAll() {
+        var changed = false;
+
+        var desc = $('[name=display_name]').val();
+        if (desc != initial_desc) {
+            changed = true;
+            $('.apply_to_all .desc .value').text(desc);
+            $('.apply_to_all .desc').show();
+            $('.apply_to_all .desc [type=checkbox]').prop('checked', true);
+        } else {
+            $('.apply_to_all .desc').hide();
+            $('.apply_to_all .desc [type=checkbox]').prop('checked', false);
+        }
+
+        var tags = $.map($('input[id^=tag_]:checked'), function(e,i) {
+            return e.value;
+        });
+        if (JSON.stringify(tags) != JSON.stringify(initial_tags)) {
+            changed = true;
+            $('.apply_to_all .tags .value').text(tags.join(', '));
+            $('.apply_to_all .tags').show();
+            $('.apply_to_all .tags [type=checkbox]').prop('checked', true);
+        } else {
+            $('.apply_to_all .tags').hide();
+            $('.apply_to_all .tags [type=checkbox]').prop('checked', false);
+        }
+
+        var category = $('[name=category]').val();
+        if (category != initial_category) {
+            changed = true;
+            $('.apply_to_all .category .value').text(category);
+            $('.apply_to_all .category').show();
+            $('.apply_to_all .category [type=checkbox]').prop('checked', true);
+        } else {
+            $('.apply_to_all .category').hide();
+            $('.apply_to_all .category [type=checkbox]').prop('checked', false);
+        }
+
+        if (changed) {
+            $('.apply_to_all').show();
+        } else {
+            $('.apply_to_all').hide();
+        }
+
+        var enabled = $('#post_processing_rule').prop('checked');
+        if (enabled) {
+            $('.apply_to_all').addClass('selected');
+        } else {
+            $('.apply_to_all').removeClass('selected');
+        }
+    }
+</script>
 </body>
 </html>
