e290e3e579d7ec2eca044f6ef1382bd83b19da4e
Guillaume Boudreau <guillaume@pommepause.com>
2020-11-21 15:01:48 -0500

    Persist is_dark choice using a cookie (valid for 1 year)

--- init.inc.php
+++ init.inc.php
@@ -2,6 +2,8 @@
 
 namespace EasyMalt;
 
+use Delight\Cookie\Cookie;
+
 require_once 'vendor/autoload.php';
 
 require_once 'functions.inc.php';
@@ -17,13 +19,11 @@ ini_set('error_reporting', E_ALL);
 
 session_start();
 
-if (@$_GET['dark'] === '1' || $_COOKIE['is_dark']) {
-    $_SESSION['is_dark'] = TRUE;
+if (isset($_GET['dark'])) {
+    $_COOKIE['is_dark'] = $_GET['dark'];
 }
-if (@$_GET['dark'] === '0') {
-    $_SESSION['is_dark'] = FALSE;
-}
-$is_dark = @$_SESSION['is_dark'];
+$is_dark = (Cookie::get('is_dark', '0') === '1');
+Cookie::setcookie('is_dark', $is_dark ? '1' : '0', time() + 365*24*60*60, '/');
 
 if (!empty(session_save_path()) && !is_writable(session_save_path())) {
     echo '<div style="color:red">Warning: Session path "'.session_save_path().'" is not writable for PHP!</div>';
